eb383057c9e9480fb99264ada0735c2c7eb342c5
Will Darkins <willdarkins@gmail.com>
2022-02-16 21:46:48 -0800

    useState set to manage info from API being passed

--- src/components/Giphy.js
+++ src/components/Giphy.js
@@ -2,15 +2,17 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios'
 
 const Giphy = () => {
+    //useState to store data from API
+    const [ content, setContent ] = useState([])
     // calling giphy api
     const giphyResponse = async () => {
         const { data } = await axios.get(`https://api.giphy.com/v1/gifs/trending?api_key=LWWhHBcSHdmy5Nshmkte5jUOYsozacsy`)
-        console.log(data)
+        // console.log(data)
+        setContent(data.data)
     }
-
+    //passing API response through useEffect
     useEffect(() => {
         giphyResponse()
-
     }, [])
 
     return (
