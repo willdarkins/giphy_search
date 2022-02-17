5f0ae05abd1051aae44d008faffea7ea4e336af1
Will Darkins <willdarkins@gmail.com>
2022-02-16 21:39:39 -0800

    logged out data from Giphy API and we all good

--- src/components/Giphy.js
+++ src/components/Giphy.js
@@ -2,15 +2,22 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios'
 
 const Giphy = () => {
-
-    const giphyResponse = () => {
-        const { data } = await axios.get(`https://api.giphy.com/v1/gifs/trending?api_key=${process.env.REACT_APP_API_KEY}`)
+    // calling giphy api
+    const giphyResponse = async () => {
+        const { data } = await axios.get(`https://api.giphy.com/v1/gifs/trending?api_key=LWWhHBcSHdmy5Nshmkte5jUOYsozacsy`)
+        console.log(data)
     }
-  return (
-    <>
-    giphy
-    </>
-  )
+
+    useEffect(() => {
+        giphyResponse()
+
+    }, [])
+
+    return (
+        <>
+            giphy
+        </>
+    )
 }
 
 export default Giphy
